dc4445982883e9c89615d8a8391d06652e807666
Sntax <andrewjrhill@gmail.com>
2015-06-23 15:39:44 +0200

    getHours bug fix

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@
 /* ===================================================== */
 
 // Define global scope.
-var fbGlobalScope = {
+var getFacebookEvents = {
 
-  init: function() {
+  init: function(fbPageId) {
 
     // Find unique page ID by viewing source of Facebook page and searching for page_id.
     // Example: View page source on https://www.facebook.com/GitHub/, CTRL+F to find 
@@ -16,7 +16,7 @@ var fbGlobalScope = {
 
     // Access Token is a combination of your App ID and App Secret. Both can be found
     // when signing up for a new Facebook App at https://developers.facebook.com/apps/
-    var accessToken = '1459046591062237|gYFhYeDeHEKXQyJLpMtrYUBiYEQ';
+    var accessToken = '1459046591062237' + '|' + 'gYFhYeDeHEKXQyJLpMtrYUBiYEQ';
 
     // List the fields you wish to be returned by the Graphing API to increase
     // performance. This will be included in a URL so do not place spaces between commas.
@@ -30,7 +30,7 @@ var fbGlobalScope = {
                     '&access_token=' + accessToken;
 
     // Get events and pass eventsURL to function.
-    fbGlobalScope.getEvents(eventsURL);
+    getFacebookEvents.getEvents(eventsURL);
   },
 
   // Define primary event retrieval function.
@@ -53,7 +53,7 @@ var fbGlobalScope = {
         // Get YYYY-MM-DD date from event date/time object returned from API.
         var eventStartDate = (eventData[i].start_time).match('^[0-9\-]*(?=T)');
         // Get HH:MM time from event date/time object returned from API.
-        var eventStartTime = eventStartTime.getHours() + ':' + ('0' + eventStartTime.getMinutes()).slice(-2);
+        var eventStartTime = eventDateTime.getHours() + ':' + ('0' + eventDateTime.getMinutes()).slice(-2);
 
         // Append data to the document body.
         $('body').append(
@@ -77,4 +77,4 @@ var fbGlobalScope = {
 }
 
 // Initialize all functions
-fbGlobalScope.init();
+getFacebookEvents.init();
